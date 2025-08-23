23c5beea7ca0cbf49aa801a78bcf5b72118af7b9
crparra4 <natacrist81@gmail.com>
2025-08-23 12:44:48 -0500

    Se hizo la estructura de Login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,12 +3,27 @@ import COVER_IMAGE from '../assets/Imagen_Login.png';
 
 const LoginForm = () => {
   return (
-    <div className="w-full h-screen flex items-start">
+    <div className="login">
         <div className="relative w-1/2 h-full flex flex-col">
             <img src={COVER_IMAGE} className="w-full h-full object-cover" />
-        </div>
-        <div className="w-full h-full bg=[#E0E0E0] flex flex-col">
-            <h1 className="text-base text-"></h1>
+        <form action="">
+            <h1>Inicio Sesion</h1>
+            <div className="input-box">
+                <input type="text" placeholder="Correo" required/>
+            </div>
+            <div className="input-box">
+                <input type="password" placeholder="Contraseña" required/>
+            </div>
+            <div className="remmerber-forgot">
+                <label><input type="checkbox"/>Acepta Terminos y Condiciones</label>
+                <a href="#">Olvido su Contraseña?</a>
+            </div>
+            <button type="submit" className="btn">Iniciar Sesion</button>
+
+            <div className="register-link">
+                <p>No tienes Cuenta? <a href="#">Registrate</a></p>
+            </div>
+        </form>    
         </div>
     </div>
   );

809dd9afd10017bf8e7ec00389c27f2bab167ca1
crparra4 <natacrist81@gmail.com>
2025-08-23 12:20:02 -0500

    para no tener confilcto

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import LoginForm from './components/LoginForm'
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <Header user={{ balance: 100, avatarUrl: reactLogo, name: 'Usuario' }} />
+    {/*<LoginForm />*/}
     </>
   )
 }
